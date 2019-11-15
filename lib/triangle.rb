d51cfebb10e168d30df3b23eed8fa84b18db71f1
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-11-15 01:51:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
     else 
       if (@side_one == @side_two) && (@side_two == @side_three)
         :equilateral
-        elsif (@side_one == @side_two) || 
+        elsif (@side_one == @side_two) || (@side_two)
       end
     end
       
